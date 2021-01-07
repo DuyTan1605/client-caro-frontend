77ac182c0371d9598ed0a63a5be50079dc4d641f
DuyTan1605 <1712148@student.hcmus.edu.vn>
2021-01-08 04:27:04 +0700

    fix bug host

--- src/actions/actionChangeInfo.js
+++ src/actions/actionChangeInfo.js
@@ -2,29 +2,31 @@ import fetch from 'cross-fetch';
 import ActionType from '../constants/actionTypes';
 import config from '../config';
 
-export function actionChangeInfo(status, message) {
+export function actionChangeInfo(status, message,userInfo) {
     return {
         type: ActionType.CHANGE_INFO,
         status,
-        message
+        message,
+        userInfo
     };
 }
 
 export default function fetchChangeInfo(name,email,avatar,type) {
-
+  //console.log(String(avatar));
     return dispatch => {
 
         const token = localStorage.getItem('token');
         const bearerToken = 'Bearer ' + token;
-
+       
         dispatch(actionChangeInfo('REQUEST', 'Please waiting for a moment...'));
 
         return fetch(config['server-domain'] + 'users/changeinfo', {
             method: 'POST',
+            mode:'cors',
             headers: {
+                'Authorization': bearerToken,
                 'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                'Authorization': bearerToken
+                'Content-Type':'application/json'
             },
             body: JSON.stringify({
                 name,
@@ -37,18 +39,21 @@ export default function fetchChangeInfo(name,email,avatar,type) {
             response => response.json(),
             error => {
                 console.log('An error occurred.', error);
-                dispatch(actionChangeInfo('FAILED', 'Error ! Please try again'));
+                dispatch(actionChangeInfo('FAILED', 'Error ! Please try again',JSON.parse(localStorage.getItem('user'))));
             }
         )
         .then(json => {
-            console.log(JSON.stringify(json))
+           console.log(json)
             let newUserInfo = json.userInfo ? localStorage.setItem('user',JSON.stringify(json.userInfo)):JSON.parse(localStorage.getItem('user'));
             if(json.token)
+            {
                 localStorage.setItem('token',json.token);
+            }
             dispatch(actionChangeInfo('SUCCESS', json.message,newUserInfo));
         })
         .catch(err => {
-            dispatch(actionChangeInfo('FAILED', err));
+            console.log(err);
+            dispatch(actionChangeInfo('FAILED', err.message,JSON.parse(localStorage.getItem('user'))));
         })
     }
   }
--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 // These exports are for localhost
 var exports_1 = {
-    'client-domain': '//localhost:3001/',
+    'client-domain': 'http://localhost:3001/',
     'server-domain': '//localhost:3000/'
 };
 
 // These exports are for uploading to heroku
 var exports_remote = {
     'client-domain': '//localhost:3001/',
-    'server-domain': 'https://caro-online-api-client.herokuapp.com/'
+    'server-domain': '//localhost:3000/'
 };
 
 var exports_final = {
@@ -15,4 +15,4 @@ var exports_final = {
     'server-domain': 'https://caro-online-api-client.herokuapp.com/'
 };
 
-export default exports_final;
+export default exports_1;
